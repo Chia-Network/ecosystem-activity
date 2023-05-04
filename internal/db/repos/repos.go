ffd78d4301e273b4966cea526ee904092c0c064a
Starttoaster <b.butler@chia.net>
2023-05-03 21:23:33 -0700

    Add required db functions for updating changing columns

--- internal/db/repos/repos.go
+++ internal/db/repos/repos.go
@@ -12,17 +12,40 @@ type Repo struct {
 	ID              int
 	Owner           string
 	Repo            string
-	CreatedAt       time.Time
 	ImportedThrough time.Time
 	FirstCommit     time.Time
 	LastCommit      time.Time
 	Notes           string
 }
 
+// SetNewRecord inserts one new record into the table
+func SetNewRecord(r Repo) ([]Repo, error) {
+	var repos []Repo
+	rows, err := db.Query(`INSERT INTO repos (owner,repo,notes) VALUES(?, ?, ?, ?, ?);`, r.Owner, r.Repo, r.Notes)
+	if err != nil {
+		return []Repo{}, fmt.Errorf("error adding repo to repos table for \"%s\" and repo \"%s\": %v", r.Owner, r.Repo, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var repo Repo
+		err := rows.Scan(&repo.ID, &repo.Owner, &repo.Repo, &repo.ImportedThrough, &repo.FirstCommit, &repo.LastCommit, &repo.Notes)
+		if err != nil {
+			return []Repo{}, fmt.Errorf("error scanning row for owner \"%s\" and repo \"%s\": %v", r.Owner, r.Repo, err)
+		}
+		repos = append(repos, repo)
+	}
+	if err := rows.Err(); err != nil {
+		return []Repo{}, fmt.Errorf("error encountered iterating through rows for owner \"%s\" and repo \"%s\": %v", r.Owner, r.Repo, err)
+	}
+
+	return repos, nil
+}
+
 // GetRowsByOwnerAndRepo returns the rows where the owner and repo both match (should be one row)
 func GetRowsByOwnerAndRepo(owner, repo string) ([]Repo, error) {
 	var repos []Repo
-	rows, err := db.Query("SELECT id,owner,repo,created_at,imported_through,first_commit,last_commit,notes FROM repos WHERE owner = ? AND repo = ?", owner, repo)
+	rows, err := db.Query("SELECT id,owner,repo,imported_through,first_commit,last_commit,notes FROM repos WHERE owner = ? AND repo = ?", owner, repo)
 	if err != nil {
 		return repos, fmt.Errorf("error querying repos table for rows by owner \"%s\" and repo \"%s\": %v", owner, repo, err)
 	}
@@ -30,7 +53,7 @@ func GetRowsByOwnerAndRepo(owner, repo string) ([]Repo, error) {
 
 	for rows.Next() {
 		var r Repo
-		err := rows.Scan(&r.ID, &r.Owner, &r.Repo, &r.CreatedAt, &r.ImportedThrough, &r.FirstCommit, &r.LastCommit, &r.Notes)
+		err := rows.Scan(&r.ID, &r.Owner, &r.Repo, &r.ImportedThrough, &r.FirstCommit, &r.LastCommit, &r.Notes)
 		if err != nil {
 			return repos, fmt.Errorf("error scanning row for owner \"%s\" and repo \"%s\": %v", owner, repo, err)
 		}
@@ -41,4 +64,31 @@ func GetRowsByOwnerAndRepo(owner, repo string) ([]Repo, error) {
 	}
 
 	return repos, nil
+}
+
+// UpdateLastCommitByID accepts a row ID and time object and updates the matching row's last_commit column to the timestamp
+func UpdateLastCommitByID(id int, ts time.Time) error {
+	_, err := db.Exec(`UPDATE repos SET last_commit='?' WHERE id='?';`, ts.Format("2006-01-02 15:04:05"), id)
+	if err != nil {
+		return fmt.Errorf("error encountered updating last_commit on row ID %d: %v", id, err)
+	}
+	return err
+}
+
+// UpdateFirstCommitByID accepts a row ID and time object and updates the matching row's first_commit column to the timestamp
+func UpdateFirstCommitByID(id int, ts time.Time) error {
+	_, err := db.Exec(`UPDATE repos SET first_commit='?' WHERE id='?';`, ts.Format("2006-01-02 15:04:05"), id)
+	if err != nil {
+		return fmt.Errorf("error encountered updating first_commit on row ID %d: %v", id, err)
+	}
+	return err
+}
+
+// UpdateImportedThroughByID accepts a row ID and time object and updates the matching row's imported_through column to the timestamp
+func UpdateImportedThroughByID(id int, ts time.Time) error {
+	_, err := db.Exec(`UPDATE repos SET imported_through='?' WHERE id='?';`, ts.Format("2006-01-02 15:04:05"), id)
+	if err != nil {
+		return fmt.Errorf("error encountered updating imported_through on row ID %d: %v", id, err)
+	}
+	return err
 }
--- internal/db/users/users.go
+++ internal/db/users/users.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/chia-network/ecosystem-activity/internal/db"
@@ -10,16 +11,73 @@ import (
 type User struct {
 	ID          int
 	Username    string
-	Email       string
-	Name        string
-	CreatedAt   time.Time
 	FirstCommit time.Time
 	LastCommit  time.Time
 	Notes       string
 }
 
 // SetNewRecord inserts one new record into the table
-func SetNewRecord(u User) error {
-	_, err := db.Exec(`INSERT INTO users (username,email,name,created_at,first_commit,last_commit,notes) VALUES(?, ?, ?, ?, ?);`, u.Username, u.Email, u.Name, u.CreatedAt, u.FirstCommit, u.LastCommit, u.Notes)
+func SetNewRecord(u User) ([]User, error) {
+	var users []User
+	rows, err := db.Query(`INSERT INTO users (username,first_commit,last_commit,notes) VALUES(?, ?, ?, ?);`, u.Username, u.FirstCommit, u.LastCommit, u.Notes)
+	if err != nil {
+		return []User{}, fmt.Errorf("error adding user to users table for \"%s\": %v", u.Username, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Username, &user.FirstCommit, &user.LastCommit, &user.FirstCommit, &user.Notes)
+		if err != nil {
+			return []User{}, fmt.Errorf("error scanning row for user \"%s\": %v", u.Username, err)
+		}
+		users = append(users, user)
+	}
+	if err := rows.Err(); err != nil {
+		return []User{}, fmt.Errorf("error encountered iterating through rows for user \"%s\": %v", u.Username, err)
+	}
+
+	return users, nil
+}
+
+// GetRowsByUsername gets a slice of rows matching a username
+func GetRowsByUsername(username string) ([]User, error) {
+	var users []User
+	rows, err := db.Query("SELECT id,username,first_commit,last_commit,notes FROM users WHERE username = ?", username)
+	if err != nil {
+		return users, fmt.Errorf("error querying users table for rows by username \"%s\": %v", username, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var r User
+		err := rows.Scan(&r.ID, &r.Username, &r.FirstCommit, &r.LastCommit, &r.Notes)
+		if err != nil {
+			return users, fmt.Errorf("error scanning row for username \"%s\": %v", username, err)
+		}
+		users = append(users, r)
+	}
+	if err := rows.Err(); err != nil {
+		return users, fmt.Errorf("error encountered iterating through rows for username \"%s\": %v", username, err)
+	}
+
+	return users, nil
+}
+
+// UpdateLastCommitByUsername accepts a username and time object and updates the matching row's last_commit column to the timestamp
+func UpdateLastCommitByUsername(username string, ts time.Time) error {
+	_, err := db.Exec(`UPDATE users SET last_commit='?' WHERE username='?';`, ts.Format("2006-01-02 15:04:05"), username)
+	if err != nil {
+		return fmt.Errorf("error encountered updating last_commit on row for %s: %v", username, err)
+	}
+	return err
+}
+
+// UpdateFirstCommitByUsername accepts a username and time object and updates the matching row's first_commit column to the timestamp
+func UpdateFirstCommitByUsername(username string, ts time.Time) error {
+	_, err := db.Exec(`UPDATE users SET first_commit='?' WHERE username='?';`, ts.Format("2006-01-02 15:04:05"), username)
+	if err != nil {
+		return fmt.Errorf("error encountered updating first_commit on row for %s: %v", username, err)
+	}
 	return err
 }
