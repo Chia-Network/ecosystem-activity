deeae9434938fe1c5ca5f6984ae2d973b3fa88f1
Starttoaster <b.butler@chia.net>
2023-05-15 21:33:53 -0700

    Centralize bot matching logic

---
+++ internal/utils/bots.go
@@ -0,0 +1,20 @@
+package utils
+
+import "strings"
+
+// Bots is used to match usernames to likely bots, and skips their commit data
+var Bots = []string{
+	"-bot",
+	"[bot]",
+	"ChiaAutomation",
+}
+
+// MatchesBot matches common bot names since we don't care for bot users in our data
+func MatchesBot(uname string) bool {
+	for _, botMatcher := range Bots {
+		if strings.Contains(strings.ToLower(uname), strings.ToLower(botMatcher)) {
+			return true
+		}
+	}
+	return false
+}
